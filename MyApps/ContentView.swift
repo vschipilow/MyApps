2ef69094ef69698d2d3d5af45cc1c2d3eee07b4b
vschipilow <victorschipilow@icloud.com>
2025-04-10 15:56:03 +1000

    Initial Commit

---
+++ MyApps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyApps
+//
+//  Created by Victor Schipilow on 10/4/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyApps/MyAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppsApp.swift
+//  MyApps
+//
+//  Created by Victor Schipilow on 10/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
